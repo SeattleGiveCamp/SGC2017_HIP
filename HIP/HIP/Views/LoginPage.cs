b185b27af85fd57ee901d7e435c905aa51597c83
software-2 <software-2@users.noreply.github.com>
2017-10-07 13:52:53 -0700

    Basic Login Page layout

--- HIP/HIP/Views/LoginPage.cs
+++ HIP/HIP/Views/LoginPage.cs
@@ -11,11 +11,79 @@ namespace HIP.Views
 	{
 		public LoginPage ()
 		{
-			Content = new StackLayout {
-				Children = {
-					new Label { Text = "The Login Screen!" }
-				}
-			};
-		}
+            var scrollview = new ScrollView();
+            Content = scrollview;
+            var layout = new StackLayout();
+            scrollview.Content = layout;
+
+
+            var logoImage = new Image
+            {
+                Source = "logo_x96.png"
+            };
+
+            var introWelcomeLabel = new Label
+            {
+                Text = "Welcome!",
+                FontSize = 30,
+                HorizontalTextAlignment = TextAlignment.Center,
+                
+                
+            };
+
+            var introDetailsLabel = new Label
+            {
+                Text = "Thanks for volunteering! Please take a moment to give us some info. I promise, you'll only have to do this once!",
+                FontSize = 15,
+                HorizontalTextAlignment = TextAlignment.Center
+
+            };
+
+            var introDetails2Label = new Label
+            {
+                Text = "Bringing more people with you? You can set them up later!",
+                FontSize = 12,
+                TextColor = Color.DarkGray,
+                HorizontalTextAlignment = TextAlignment.Center
+
+            };
+
+            var firstNameEntry = new Entry
+            {
+                Placeholder = "First Name",
+                IsPassword = false
+            };
+
+            var lastNameEntry = new Entry
+            {
+                Placeholder = "Last Name",
+                IsPassword = false
+            };
+
+            var emailEntry = new Entry
+            {
+                Placeholder = "Email",
+                IsPassword = false
+            };
+
+            
+            var doneButton = new Button
+            {
+                Text = "Done",
+                VerticalOptions = LayoutOptions.Start,
+                HorizontalOptions = LayoutOptions.FillAndExpand
+            };
+
+
+            layout.Children.Add(logoImage);
+            layout.Children.Add(introWelcomeLabel);
+            layout.Children.Add(introDetailsLabel);
+            layout.Children.Add(introDetails2Label);
+            layout.Children.Add(firstNameEntry);
+            layout.Children.Add(lastNameEntry);
+            layout.Children.Add(emailEntry);
+            layout.Children.Add(doneButton);
+            layout.Spacing = 10;
+        }
 	}
 }
