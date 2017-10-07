493cfc5a8a16026d9f571a4ac8b185f6143f9e1f
asazonov <aleksei.sazonov@smartsheet.com>
2017-10-07 14:10:32 -0700

    add EventViewModel and bind list items to it,   add packages for calendar and permissions, add calendar service

---
+++ HIP/HIP/ViewModels/EventViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Input;
+using FormsToolkit;
+using HIP.Models;
+using HIP.Services;
+using Xamarin.Forms;
+
+namespace HIP.ViewModels
+{
+	public class EventViewModel
+	{
+		public EventViewModel(Event model)
+		{
+			Event = model;
+			AddToCalendar = new Command(() => {
+				MessagingService.Current.SendMessage<MessagingServiceAlert>("message", new MessagingServiceAlert
+				{
+					Title = "Event Creation",
+					Message = "Pfff",
+					Cancel = "OK"
+				});
+
+                //CalendarService.AddReminderAsync();
+			});
+
+            Name = model.Name;
+            Description = model.Description;
+            Date = model.Date;
+		}
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Date { get; set; }
+
+		public Event Event { get; set; }
+
+		public ICommand AddToCalendar { get; }
+	}
+}
