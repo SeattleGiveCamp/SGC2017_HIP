63fdd1161e894c1d12bdb425db157e434b36c0cf
software-2 <projects@anthonybryan.net>
2017-10-07 11:25:05 -0700

    Created basic User and Event models

---
+++ HIP/HIP/Models/Event.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HIP.Models
+{
+    class Event
+    {
+        public string Name { get; }
+        public string Description { get; }
+        public Boolean IsRecurring { get; }
+
+        public DateTime NextTime { get; }  //TODO: Currently statically setting these values. Need to write logic to handle these correctly
+        public Double DurationInHours { get; }
+
+        private DateTime startTime;
+        private DateTime endTime;
+
+        //For a single-time event
+        public Event(string name, string description, DateTime singleEventDate, Double durtionInHours)
+        {
+            Name = name;
+            Description = description;
+            IsRecurring = false;
+
+            //TODO: Currently statically setting these values. Need to write logic to handle these correctly
+            NextTime = singleEventDate;
+            DurationInHours = durtionInHours;
+        }
+
+        //For a recurring event
+        
+    }
+}
---
+++ HIP/HIP/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HIP.Models
+{
+    class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
