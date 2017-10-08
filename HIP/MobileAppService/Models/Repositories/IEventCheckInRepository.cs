0f5a7d72a13d970ea17907dd74facb3b10dd75df
matt <matt>
2017-10-07 21:29:48 -0700

    adding event check in capabilities

---
+++ HIP/MobileAppService/Models/Repositories/IEventCheckInRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HIP.MobileAppService.Models.Repositories
+{
+    public interface IEventCheckInRepository
+    {
+        void Add(EventCheckInModel item);
+		EventCheckInModel Get(string email);
+		IEnumerable<EventCheckInModel> GetAll();
+    }
+}
