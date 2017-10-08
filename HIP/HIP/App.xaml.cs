d9dbc706fbac572bad4d0a8f0a59846312293db9
software-2 <software-2@users.noreply.github.com>
2017-10-08 11:28:15 -0700

    Null check

--- HIP/HIP/App.xaml.cs
+++ HIP/HIP/App.xaml.cs
@@ -30,7 +30,7 @@ namespace HIP
                 DependencyService.Register<CloudDataStore>();
 
             //Only show the settings screen initially if the user has not previously provided an email
-            if (Application.Current.Properties.ContainsKey("email"))
+            if (Application.Current.Properties != null && Application.Current.Properties.ContainsKey("email"))
             {
                 if (Xamarin.Forms.Device.RuntimePlatform == Xamarin.Forms.Device.iOS)
                     MainPage = new UpcomingProgramsPage();
