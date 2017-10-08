389bb496b8475288bd8bb1a82086722bac0e903b
matt <matt>
2017-10-08 12:25:07 -0700

    saving event checkins

--- HIP/MobileAppService/Models/Repositories/IEventCheckInRepository.cs
+++ HIP/MobileAppService/Models/Repositories/IEventCheckInRepository.cs
@@ -6,7 +6,6 @@ namespace HIP.MobileAppService.Models.Repositories
     public interface IEventCheckInRepository
     {
         void Add(EventCheckInModel item);
-		EventCheckInModel Get(string email);
 		IEnumerable<EventCheckInModel> GetAll();
     }
 }
