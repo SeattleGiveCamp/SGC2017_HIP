e15ec5a1fba3d156378589e5770a91b6094995e0
matt <matt>
2017-10-08 11:34:03 -0700

    implementing user APIs

--- HIP/MobileAppService/Models/UserModel.cs
+++ HIP/MobileAppService/Models/UserModel.cs
@@ -3,28 +3,27 @@ using System.ComponentModel.DataAnnotations;
 
 namespace HIP.MobileAppService.Models
 {
-    public class UserModel
-    {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        [Key]
-        public string Email { get; set; }
+	public class UserModel
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string Email { get; set; }
+		public string ParentEmail { get; set; }
 
-		public UserModel()
-        {}
+		public UserModel(string parentEemail)
+		{
+			FirstName = "";
+			LastName = "";
+            Email = "";
+            ParentEmail = parentEemail;
+		}
 
-        public UserModel(string email)
-        {
-            FirstName = "";
-            LastName = "";
-            Email = email;
-        }
-
-        public UserModel(string email, string firstName, string lastName)
-        {
-            FirstName = firstName;
-            LastName = lastName;
-            Email = email;
-        }
-    }
+		public UserModel(string email, string firstName, string lastName)
+		{
+			FirstName = firstName;
+			LastName = lastName;
+			Email = email;
+			ParentEmail = "";
+		}
+	}
 }
