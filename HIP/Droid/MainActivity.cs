d426d0b852034a3e25f3337993d04c69e5089392
software-2 <software-2@users.noreply.github.com>
2017-10-08 09:55:00 -0700

    Added Android icons

--- HIP/Droid/MainActivity.cs
+++ HIP/Droid/MainActivity.cs
@@ -11,7 +11,7 @@ using Plugin.CurrentActivity;
 
 namespace HIP.Droid
 {
-    [Activity(Label = "HIP.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "HIP", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
